534edcac10419b87c989dea1cd3d4363df78e2b6
Roma <rossraijin@gmail.com>
2023-02-14 15:00:42 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+// MOCK DB
+const mainMenu = [
+  {postiton: 1, value: 'Option'},
+  {postiton: 2, value: 'Option'},
+  {postiton: 3, value: 'Option'},
+  {postiton: 4, value: 'Option'},
+  {postiton: 5, value: 'Option'},
+  {postiton: 6, value: 'Option'},
+  {postiton: 7, value: 'Option'},
+  {postiton: 8, value: 'Option'}
+];
+
+const myZagolovokSaita = 'Hello My SIte';
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>{myZagolovokSaita}</h1>
+      <ul className=''>
+        {
+          mainMenu.map((item, i) => {
+            console.log('item: ', item, 'i: ', i);
+            return (
+              <li className='' key={`top-menu-item-${i}`}>
+                {item.value} <span>Position: {item.postiton}</span> | <span>Index: {i}</span>
+              </li>
+            )
+          })
+        }
+      </ul>
     </div>
   );
 }
