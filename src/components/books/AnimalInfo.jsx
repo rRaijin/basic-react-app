fdffcc3cc4c0f2f7876dae0237f31587c4e210cd
Roma <rossraijin@gmail.com>
2023-05-28 11:54:58 +0300

    28-05

--- src/components/books/AnimalInfo.jsx
+++ src/components/books/AnimalInfo.jsx
@@ -1,11 +1,15 @@
-// const AnimalInfo = ({ animal }) => {
-//   return (
-//     <div>
-//       <h4>Kind: {animal.kind}</h4>
-//       <p>Name: {animal.name}</p>
-//       <p>Age: {animal.age}</p>
-//     </div>
-//   );
-// };
+// dump-component & smart-component
 
-// export default AnimalInfo;
+const AnimalInfo123 = ({ animal }) => {
+  return (
+    <div>
+      <h4>Kind: {animal.kind}</h4>
+      <p>Name: {animal.name}</p>
+      <p>Age: {animal.age}</p>
+    </div>
+  );
+};
+
+export const myConst = 1
+
+export default AnimalInfo123;
--- src/components/books/Blink.jsx
+++ src/components/books/Blink.jsx
@@ -1,7 +1,7 @@
 import { Component } from "react";
 import MyInputComponent from "./MyInput";
 import MyComponentSome3 from "./Some3";
-// import AnimalInfo from "./Animal-component";
+import AnimalInfo99, {myConst} from "./AnimalInfo";
 // import AnimalData from "./Animal-Data";
 
 // const [myInputName, setMyInputName] = useState('');
@@ -15,10 +15,12 @@ class MyComponent extends Component {
         this.state = {
             position: 1,
             a: 0,
-            Heart: 'https://e7.pngegg.com/pngimages/725/818/png-clipart-heart-simple-red-people-heart-outline.png'
-
+            Heart: 'https://e7.pngegg.com/pngimages/725/818/png-clipart-heart-simple-red-people-heart-outline.png',
+            fontSize: 20,
+            textareaValue: ''
         }
         this.some1 = this.some1.bind(this);
+        this.changeFontSizeHandler = this.changeFontSizeHandler.bind(this);
         const x = 45;
     }
 
@@ -76,6 +78,19 @@ class MyComponent extends Component {
         this.setState({Heart : newHeart});
     }
 
+    changeFontSizeHandler() {
+        this.setState({fontSize: 30})
+    }
+
+    changeFontSizeHandlerReverse() {
+        this.setState({fontSize: 20})
+    }
+
+    changeTextareaValueHandler = (e) => {
+        const val = e.target.value;
+        console.log('val: ', val);
+        this.setState({textareaValue: val});
+    }
 
     // единственный обязательный метод - render
     render() {
@@ -85,14 +100,34 @@ class MyComponent extends Component {
         // console.log('Появление-2', this.state.position)
         console.log('this.state.animal: ', this.state.animal)
 
-        // домашняя работа
-        // const myAnimal = {kind: 'cat', name: 'klyaksa', age: 1}
+        // домашняя работа 1
+        const animals = [
+            {kind: 'cat', name: 'klyaksa', age: 1},
+            {
+                kind: 'Dog',
+                name: 'Persik',
+                age: 2
+            }
+        ];
+
+        // домашняя работа 2.1 Мапнуть массив из двух объектов, на каждой итерации вернуть собственный компонент
+        const plants = [{kind: "tree", name: "elka", height: 3}, {kind: "flower", name: "romashka", height: 0.2}]
+
+        // домашняя работа 2.2 Написать <input>, просто менящий значение в state и выводящий его console.log, хэндлер не на чистой функции
 
         return (
             <div className={`container self-slider`}>
+                {/* <p className={`font-20`} onMouseEnter={this.changeFontSizeHandler}>Some text for example</p> */}
+                <p
+                    className={`font-${this.state.fontSize}`} // Динамический параметр из стейта
+                    onMouseEnter={this.changeFontSizeHandler}
+                    onMouseLeave={this.changeFontSizeHandlerReverse.bind(this)}>
+                    Some text for example
+                </p>
+
                 {/* <img src={`/slides/slide_${this.state.position}.jpg`} alt="" /> */}
                 {/* <Sx pos={this.state.position}/> */}
-                <button onClick={this.some1}>
+                {/* <button onClick={this.some1}>
                     test
                 </button>
                 <button onClick={this.some2.bind(this)}>
@@ -103,11 +138,43 @@ class MyComponent extends Component {
                 </button>
                 <MyInputComponent/>
                 <MyComponentSome3/>
-                {/* <div>
+                <div>
                     <h4>Kind: {myAnimal.kind}</h4>
                     <p>Name: {myAnimal.name}</p>
                     <p>Age: {myAnimal.age}</p>
                 </div> */}
+
+{/* homework task 1 */}
+                {
+                    plants.map((item, i) => {
+                        return (
+                            <div>
+                                {/* <p>{item.kind}</p> */}
+                                {/* кнопка по нажатию меняет цвет заголовка h1 на любой другой и при повторном длеает это обратно */}
+                                {/* <button onClick={}>
+                                    Change color
+                                </button> */}
+                            </div>
+                        )
+                    })
+                }
+
+
+{/* homework task 2 */}
+                <textarea onChange={this.changeTextareaValueHandler} value={this.state.textareaValue}></textarea>
+                {
+                    this.state.textareaValue === 'Пылесос' &&
+                    <div>РАБОТАЕТ!!!!!!</div>
+                }
+
+
+                {
+                    animals.map((animalItem, i) => {
+                        return (
+                            <AnimalInfo99 animal={animalItem} key={`animal-${i}`}/>
+                        )
+                    })
+                }
 
                 <div>
                     <button className="border-0 back-white " onClick={this.HeartClick}>
