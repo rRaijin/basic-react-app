b0be1235fa223707ad4db9418d5b90caf453bb65
Svyatoslav <savchuksr@gmail.com>
2023-09-30 07:37:40 +0300

    30.09.23

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,75 +1,109 @@
+
 import React, { useState, useEffect } from "react";
 
 export default () => {
-    const [item, setItem] = useState();
-    const [selectedId, setSelectedId] = useState(0);
-    const [hasPrevPost, setHasPrevPost] = useState(false);
-    const [firstTransition, setFirstTransition] = useState(false);
+     const [posts, setPosts] = useState([]);
+     const [selectedId, setSelectedId] = useState(0);
+     const [hasPrevPost, setHasPrevPost] = useState(false);
+     const [firstTransition, setFirstTransition] = useState(false);
+     const [comments, setComments] = useState([]);
 
-    const getItem = (way) => {
-        const selectedVal = way === "next" ? selectedId + 1 : selectedId - 1;
-        fetch(`https://jsonplaceholder.typicode.com/posts/${selectedVal}`, {
-          method: "GET",
+     const getPost = (postId) => {
+         fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`, {
+           method: "GET",
+         })
+         .then((response) => {
+           if (response.status === 404) {
+             setHasPrevPost(false);
+           } else if (response.status === 200) {
+             return response.json();
+           }
+         })
+         .then((data) => {
+           if (data && data !== undefined && data !== "undefined") {
+             const updatedPosts = [...posts, data];
+             setPosts(updatedPosts);
+             setSelectedId(postId);
+             if (postId === 1 && !firstTransition) {
+               setHasPrevPost(false);
+               setFirstTransition(true);
+             } else {
+               setHasPrevPost(true);
+             }
+           } else {
+             console.log("NO DATA!");
+           }
+         });
+     };
+
+     const getCommentsForPost = (postId) => {
+        fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`, {
+            method: "GET",
         })
         .then((response) => {
-            console.log("resp: ", response);
-            if (response.status === 404) {
-                setItem();
-                setSelectedId(0);
-                setHasPrevPost(false);
-            } else if (response.status === 200) {
-                return response.json();
-            }
+          if (response.status === 200) {
+              return response.json();
+          }
         })
         .then((data) => {
-            if (data && data !== undefined && data !== "undefined") {
-                  console.log("data: ", data);
-                  setItem(data);
-                  setSelectedId(selectedVal);
-              if (selectedVal === 1 && !firstTransition) {
-                  setHasPrevPost(false);
-                  setFirstTransition(true);
-              } else {
-                  setHasPrevPost(true);
-              }
-            } else {
-              console.log("NO DATA!");
-            }
+          if (data && data !== undefined && data !== "undefined") {
+              setComments(data);
+          } else {
+              console.log("NO COMMENTS DATA!");
+          }
         });
-    };
-// 1. Не показывать кнопку prev если предыдущий итем не может быть запрошен( т.е. не должно быть /posts/0)
-// 2. При начальном монтировании компонента запросить первый пост, кнопки prev еще не будет, только next, появится только после перехода на 2й пост
-// 3. Вывести для полученного поста все его комментарии.(name, email, body)
+  };
+
   useEffect(() => {
-    // При начальном монтировании компонента запросить первый пост
-    getItem("next");
+    // При начальном монтировании компонента запросить первый пост и его комментарии
+    getPost(1);
+    getCommentsForPost(1);
   }, []);
+
+  useEffect(() => {
+    // При изменении выбранного поста, запросить его комментарии
+    getCommentsForPost(selectedId);
+  }, [selectedId]);
 
   return (
       <div className="">
           <div className="">
               {hasPrevPost && (
-                <button className="" onClick={() => getItem("prev")}>
+                <button className="" onClick={() => getPost(selectedId - 1)}>
                   PREV
                 </button>
               )}
-              <button className="" onClick={() => getItem("next")}>
+              <button className="" onClick={() => getPost(selectedId + 1)}>
                 NEXT
               </button>
+          </div>
+          {posts.length > 0 ? (
+            <div className="">
+              <h1>{posts[selectedId - 1].title}</h1>
+              <p>{posts[selectedId - 1].id}</p>
+              <p>{posts[selectedId - 1].body}</p>
             </div>
-            {item ? (
-              <div className="">
-                <h1>{item.title}</h1>
-                <p>{item.id}</p>
-                <p>{item.body}</p>
-              </div>
-              ) : (
-                <div>No data</div>
-              )}
-      </div>
-  );
+          ) : (
+            <div>No data</div>
+          )}
 
+          {comments.length > 0 ? (
+            <div>
+              <h2>Comments:</h2>
+              {comments.map((comment) => (
+                <div key={comment.id}>
+                  <p>{comment.name}</p>
+                  <p>{comment.body}</p>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div>No comments</div>
+          )}
+    </div>
+  );
 };
+
 
 
 // export default () => {
