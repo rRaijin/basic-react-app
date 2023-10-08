f4a8f1b376f931c81580bee0eaf55d73df752e0f
Roma <rossraijin@gmail.com>
2023-10-08 13:27:48 +0300

    08-10-2023

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,49 @@
-
 import React, { useState, useEffect } from "react";
+
+
+// const x = {a: {b:1}}
+// console.log(a.b) // a["b"]
+function changeEmailFunc(items) {
+    const results = [];
+    for (let i = 0; i < items.length; i++) {
+        const el = {...items[i]};
+        switch (el.email) {
+            case 'Eliseo@gardner.biz':
+                el.email = 'Sincere@april.biz';
+                break;
+            case 'Jayne_Kuhic@sydney.com':
+                el.email = 'Shanna@melissa.tv';
+                break;
+            case 'Nikita@garfield.biz':
+                el.email = 'Nathan@yesenia.net';
+                break;
+            case 'Hayden@althea.biz':
+                el.email = 'Julianne.OConner@kory.org';
+                break;
+            case 'Lew@alysha.tv':
+                el.email = 'Lucio_Hettinger@annie.ca';
+                break;
+        }
+        results.push(el);
+    }
+    return results;
+}
+
+
+
+// Task 1: Страницу поста оформить, вывести текст и список комментариев, и кнопку для каждого комментария - получить данные его автора
+// Если данные пришли добавить автора в стейт в список авторов и вывести под комментарием его данные
+// Task 2: После того, как запросили автора комментария и вывели его данные - под ними вывели и его заметки
+
+
+// Оформить страницу так:
+// 1) у каждого абзаца сделать только первую букву очень большой, жирной, и красного цвета
+// 2) после каждого предложения долждна стоять точка и пробел, а первое слово в этом предложении должно начинаться с большой буквы
+// 3) оформить красиво заголовок - текст подчеркнутый и с наколоном, голубого цвета и также начинается с большой буквы
+// 4) для комментариев свойство "name" выводим полностью текст, а "body" обрезаем до 30(50) символов и добавляем "... more"
+// 5) для автора помимо имени вывести city, street & suite
+// 6) под именем автора вывести список его заметок, не весь, первые пять (https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/slice)
+// 7) список тодос идет в столбик - 5 строк, напротив названия заметки показать статус ее выполнения(если да - V, если нет - Х)
 
 export default () => {
     const [posts, setPosts] = useState([]);
@@ -7,7 +51,7 @@ export default () => {
     const [hasPrevPost, setHasPrevPost] = useState(false);
     const [firstTransition, setFirstTransition] = useState(false);
     const [comments, setComments] = useState([]);
-    const [authors, setAuthors] = useState({});
+    const [authors, setAuthors] = useState([]);
 
     const getPost = (postId) => {
         // Если использовать filter() Фильтруем по критерию(id), если в массиве не будет объекта - вернется пустой массив, 
@@ -64,7 +108,8 @@ export default () => {
               if (data && data !== undefined && data !== "undefined") {
                   const updatedComments = [...comments.filter(comm => comm.postId !== postId), ...data];
                   // console.log('upda: ', updatedComments)
-                  setComments(updatedComments);
+                  const mutatedList = changeEmailFunc(updatedComments);
+                  setComments(mutatedList);
               } else {
                   console.log("NO COMMENTS DATA!");
               }
@@ -72,28 +117,50 @@ export default () => {
         }
     };
 
-    const getAuthorData = (authorId) => {
-        if (!authors[authorId]) {
-            fetch(`https://jsonplaceholder.typicode.com/users/${authorId}`, {
-                method: "GET",
-            })
-            .then((response) => {
-                if (response.status === 200) {
-                    return response.json();
-                }
-            })
-            .then((data) => {
-                if (data && data !== undefined && data !== "undefined") {
-                    setAuthors((prevAuthors) => ({
-                        ...prevAuthors,
-                        [authorId]: data,
-                    }));
-                } else {
-                    console.log("NO AUTHOR DATA!");
-                }
-            });
-        }
+    const getAuthorsData = () => {
+        fetch(`https://jsonplaceholder.typicode.com/users`, {
+            method: "GET",
+        })
+        .then((response) => {
+            if (response.status === 200) {
+                return response.json();
+            }
+        })
+        .then((data) => {
+            if (data && data !== undefined && data !== "undefined") {
+                // console.log('authors: ', data);
+                setAuthors(data);
+            } else {
+                console.log("NO AUTHOR DATA!");
+            }
+        });
     };
+
+    // Эта функция сейчас работает 1 раз при загрузке компонента на получение тодос для первого автора
+    // а нужно по нажатию кнопки на авторе запрашивать его тодос, данные вывести в массив из таких объектов:
+    // [{authorId: 1, todos: data}, {authorId: 2, todos: data}]
+    const getTodos = () => {
+        fetch(`https://jsonplaceholder.typicode.com/todos?userId=1`, {
+            method: "GET",
+        })
+        .then((response) => {
+            if (response.status === 200) {
+                return response.json();
+            }
+        })
+        .then((data) => {
+            if (data && data !== undefined && data !== "undefined") {
+                console.log('todos: ', data);
+            } else {
+                console.log("NO TODOS DATA!");
+            }
+        });
+    };
+
+    useEffect(() => {
+        getAuthorsData();
+        getTodos();
+    }, []);
 
     useEffect(() => {
         getPost(1);
@@ -126,8 +193,30 @@ export default () => {
                 currentPost ?
                 <div className="mb-20px">
                     <h1>{currentPost.title}</h1>
-                    <p>{currentPost.id}</p>
-                    <p>{currentPost.body}</p>
+                    <p>
+                        <span>Молоко.</span>
+                        <span>{currentPost.body}.</span>
+                        <span>{currentPost.body}</span>
+                        <span>{currentPost.body}</span>
+                        <span>{currentPost.body}</span>
+                        <span>{currentPost.body}</span>
+                    </p>
+                    <p>
+                        <span>Хлеб.</span>
+                        <span>{currentPost.body}</span>
+                        <span>{currentPost.body}</span>
+                        <span>{currentPost.body}</span>
+                        <span>{currentPost.body}</span>
+                        <span>{currentPost.body}</span>
+                    </p>
+                    <p>
+                        <span>Груша.</span>
+                        <span>{currentPost.body}</span>
+                        <span>{currentPost.body}</span>
+                        <span>{currentPost.body}</span>
+                        <span>{currentPost.body}</span>
+                        <span>{currentPost.body}</span>
+                    </p>
                 </div>
                 :
                 <div>No data</div>
@@ -136,19 +225,24 @@ export default () => {
             {commentsList.length > 0 ? (
                 <div className="mt-20px" key={`comments-list-for-post-${selectedId}`}>
                     <h2>Comments:</h2>
-                    {commentsList.map((comment) => (
-                        <div className="comment" key={comment.id}>
-                            <p className="fz16px-mb10px">{comment.name}</p>
-                            <p className="fz16px-mb10px">{comment.body}</p>
-                            <button className="author-button" onClick={() => getAuthorData(comment.userId)}>Get Author Data</button>
-                            {authors[comment.userId] && (
-                                <div className="author-info">
-                                    <p className="fz16px-mb10px">Author: {authors[comment.userId].name}</p>
-                                    <p className="fz16px-mb10px">Email: {authors[comment.userId].email}</p>
+                    {
+                        commentsList.map((comment) => {
+                            // console.log('comm: ', comment);
+                            const owner = authors.find(a => a.email === comment.email);
+                            console.log('owner: ', owner)
+                            return (
+                                <div className="comment" key={comment.id}>
+                                    <p className="fz16px-mb10px">{comment.name}</p>
+                                    <p className="fz16px-mb10px">{comment.body}</p>
+                                    {owner && (
+                                        <div className="author-info">
+                                            <p className="fz16px-mb10px">Author: {owner.name}</p>
+                                        </div>
+                                    )}
                                 </div>
-                            )}
-                        </div>
-                    ))}
+                            )
+                        })
+                    }
                 </div>
             ) : (
                 <div>No comments</div>
