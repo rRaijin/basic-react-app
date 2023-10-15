2e40a74c5b4553ff8f8a4f33cfb6546a2134d5ab
Roma <rossraijin@gmail.com>
2023-10-15 12:34:33 +0300

    classwork-15-10

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,11 +6,10 @@ import React, { useState, useEffect } from "react";
 function changeEmailFunc(items) {
     const results = [];
     for (let i = 0; i < items.length; i++) {
-        const el = {...items[i]};
-        switch (el.email) {
-            case 'Eliseo@gardner.biz':
-                el.email = 'Sincere@april.biz';
-                break;
+        const el = {...items[i]}; // {email: 'some string', x: 1}
+
+        // Внутрь оператора switch-case передается некоторое значение, которое сравнивается с каждым кейсом до первого соответствия
+        switch (el.email) { // 'Eliseo@gardner.biz'
             case 'Jayne_Kuhic@sydney.com':
                 el.email = 'Shanna@melissa.tv';
                 break;
@@ -23,27 +22,49 @@ function changeEmailFunc(items) {
             case 'Lew@alysha.tv':
                 el.email = 'Lucio_Hettinger@annie.ca';
                 break;
+            case 'Eliseo@gardner.biz':
+                el.email = 'Sincere@april.biz';
+                break;
+
+            default:
+                el.email = 'qwerty';
+                break;
         }
+
+        // if (el.email === 'Jayne_Kuhic@sydney.com') {
+        //     el.email = 'Shanna@melissa.tv';
+        // } else if (el.email === 'Nikita@garfield.biz') {
+        //     el.email = 'Nathan@yesenia.net';
+        // } else if (el.email === 'Hayden@althea.biz') {
+        //     el.email = 'Julianne.OConner@kory.org';
+        // } else if (el.email === 'Lew@alysha.tv') {
+        //     el.email = 'Lucio_Hettinger@annie.ca';
+        // } else if (el.email === 'Eliseo@gardner.biz') {
+        //     el.email = 'Sincere@april.biz';
+        // } else {
+        //     el.email = 'qwerty';
+        // }
+
         results.push(el);
     }
     return results;
 }
 
 
-
+// 01.10 TASKS
 // Task 1: Страницу поста оформить, вывести текст и список комментариев, и кнопку для каждого комментария - получить данные его автора
-// Если данные пришли добавить автора в стейт в список авторов и вывести под комментарием его данные
-// Task 2: После того, как запросили автора комментария и вывели его данные - под ними вывели и его заметки
+// Если данные пришли добавить автора в стейт в список авторов и вывести под комментарием его данные - YES
+// Task 2: После того, как запросили автора комментария и вывели его данные - под ними вывели и его заметки - NO
 
-
+// 08.10 TASKS
 // Оформить страницу так:
-// 1) у каждого абзаца сделать только первую букву очень большой, жирной, и красного цвета
-// 2) после каждого предложения долждна стоять точка и пробел, а первое слово в этом предложении должно начинаться с большой буквы
-// 3) оформить красиво заголовок - текст подчеркнутый и с наколоном, голубого цвета и также начинается с большой буквы
-// 4) для комментариев свойство "name" выводим полностью текст, а "body" обрезаем до 30(50) символов и добавляем "... more"
-// 5) для автора помимо имени вывести city, street & suite
-// 6) под именем автора вывести список его заметок, не весь, первые пять (https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/slice)
-// 7) список тодос идет в столбик - 5 строк, напротив названия заметки показать статус ее выполнения(если да - V, если нет - Х)
+// 1) у каждого абзаца сделать только первую букву очень большой, жирной, и красного цвета - YES/NO
+// 2) после каждого предложения долждна стоять точка и пробел, а первое слово в этом предложении должно начинаться с большой буквы - NO
+// 3) оформить красиво заголовок - текст подчеркнутый и с наколоном, голубого цвета и также начинается с большой буквы - YES
+// 4) для комментариев свойство "name" выводим полностью текст, а "body" обрезаем до 30(50) символов и добавляем "... more" - NO
+// 5) для автора помимо имени вывести city, street & suite - YES
+// 6) под именем автора вывести список его заметок, не весь, первые пять (https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/slice) - NO
+// 7) список тодос идет в столбик - 5 строк, напротив названия заметки показать статус ее выполнения(если да - V, если нет - Х) - NO
 
 export default () => {
     const [posts, setPosts] = useState([]);
@@ -53,7 +74,7 @@ export default () => {
     const [comments, setComments] = useState([]);
     const [authors, setAuthors] = useState([]);
     const [showFullText, setShowFullText] = useState(false);
-    const [notes, setNotes] = useState({});
+    const [notes, setNotes] = useState([]);
 
     const getNotesForAuthor = (authorId) => {
         fetch(`https://jsonplaceholder.typicode.com/todos${authorId}`, {
@@ -163,8 +184,8 @@ export default () => {
     // Эта функция сейчас работает 1 раз при загрузке компонента на получение тодос для первого автора
     // а нужно по нажатию кнопки на авторе запрашивать его тодос, данные вывести в массив из таких объектов:
     // [{authorId: 1, todos: data}, {authorId: 2, todos: data}]
-    const getTodos = () => {
-        fetch(`https://jsonplaceholder.typicode.com/todos?userId=1`, {
+    const getTodos = (userId) => {
+        fetch(`https://jsonplaceholder.typicode.com/todos?userId=${userId}`, {
             method: "GET",
         })
         .then((response) => {
@@ -174,7 +195,8 @@ export default () => {
         })
         .then((data) => {
             if (data && data !== undefined && data !== "undefined") {
-                console.log('todos: ', data);
+                // console.log('todos: ', data);
+                setNotes([...notes, {authorId: userId, todos: data}]);
             } else {
                 console.log("NO TODOS DATA!");
             }
@@ -185,7 +207,6 @@ export default () => {
 
     useEffect(() => {
         getAuthorsData();
-        getTodos();
     }, []);
 
     useEffect(() => {
@@ -209,8 +230,6 @@ export default () => {
     const currentPost = posts.find(p => p.id === selectedId);
     const commentsList = comments.filter(comm => comm.postId === selectedId);
 
-    
-
     const toggleShowFullText = () => {
         setShowFullText(!showFullText);
     };
@@ -221,6 +240,8 @@ export default () => {
         }
         return text;
     };
+
+    console.log('notes: ', notes);
 
     return (
         <div className="">
@@ -291,7 +312,9 @@ export default () => {
                         commentsList.map((comment) => {
                             // console.log('comm: ', comment);
                             const owner = authors.find(a => a.email === comment.email);
-                            console.log('owner: ', owner)
+                            // console.log('owner: ', owner);
+                            const alreadyFetchedNotes = notes.find(note => note.authorId === owner.id);
+                            const hasNotes = alreadyFetchedNotes && alreadyFetchedNotes.todos.length > 0; // есть записи
                             return (
                                 <div className="comment" key={comment.id}>
                                     <p className="fz16px-mb10px">{comment.name}</p>
@@ -302,7 +325,35 @@ export default () => {
                                             <p>City: {owner.address.city}</p>
                                             <p>Street: {owner.address.street}</p>
                                             <p>Suite: {owner.address.suite}</p>
-                                            <p>Notes:</p>
+
+                                            {
+                                                alreadyFetchedNotes ?
+                                                <div>
+                                                    {
+                                                        hasNotes ?
+                                                        <ul className="flex flex-col">
+                                                            {
+                                                                alreadyFetchedNotes.todos.map((note, i) => {
+                                                                    // console.log('note: ', note)
+                                                                    return (
+                                                                        <li className="flex">
+                                                                            <p style={{color: 'green'}}>{note.title}</p>
+                                                                            <p style={{color: 'red'}}>{note.completed ? 'V' : 'X'}</p>
+                                                                        </li>
+                                                                    )
+                                                                })
+                                                            }
+                                                        </ul> :
+                                                        <p>No notes for this author.</p>
+                                                    }
+                                                </div> :
+                                                <button onClick={() => getTodos(owner.id)}>
+                                                    Get todos
+                                                </button>
+                                            }
+
+
+                                            {/* <p>Notes:</p>
                                             {notes[owner.id] && notes[owner.id].length > 0 ? (
                                                 notes[owner.id].slice(0, 5).map((note, index) => (
                                                     <div key={index} className="note">
@@ -312,7 +363,7 @@ export default () => {
                                                 ))
                                             ) : (
                                                 <div>No notes for this author.</div>
-                                            )}
+                                            )} */}
                                         </div>
                                     )}
 
